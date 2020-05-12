23b48b3bfc0cbfd8b9b139ae6be4337bbd962455
ashish <katnoria.ashish@gmail.com>
2020-05-12 19:24:34 +0800

    Added download script

---
+++ download_checkpoints.sh
@@ -0,0 +1,6 @@
+# Download 448px model checkpoints
+wget https://sprinkle.nyc3.digitaloceanspaces.com/checkpoints/crowd-density-v1/vgg16baseline_448_400ep_private-1e-05-0.052820200430.pt -P src/ckpt
+wget https://sprinkle.nyc3.digitaloceanspaces.com/checkpoints/crowd-density-v1/vgg16decoder_448_400ep_private-1e-05-0.050320200501.pt -P src/ckpt
+# Download 224px model checkpoints
+wget https://sprinkle.nyc3.digitaloceanspaces.com/checkpoints/crowd-density-v1/vggbaseline-1e-05-0.062620200426.pt -P src/ckpt
+wget https://sprinkle.nyc3.digitaloceanspaces.com/checkpoints/crowd-density-v1/vggdecoder-1e-05-0.057920200427.pt -P src/ckpt
